329b5dac6565c8a8923fb31b81d9f79c4854587d
Sebastian-V-Apply-Digital <sebastian.villarroel@applydigital.com>
2025-11-28 23:21:08 -0500

    Add @emotion and MUI dependencies to project

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,156 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import {
+    Box,
+    Button,
+    Card,
+    CardActions,
+    CardContent,
+    CardMedia,
+    Container,
+    Typography
+} from "@mui/material";
+import AppBar from "./surfaces/AppBar.tsx";
 
-function App() {
-  const [count, setCount] = useState(0)
+export const App = () => {
+    return (
+        <div>
+            <AppBar/>
+            <Box padding={2} component="section" display={"flex"} justifyContent={'center'}>
+                <Typography variant="h2" >
+                    My Projects
+                </Typography>
+            </Box>
+            <Container
+                sx={{
+                    paddingTop: { 'xs': 4, 'md': 4},
+                    paddingX: { 'xs': 0, 'md': 4},
+                    display: 'flex',
+                    flexDirection: {xs: 'column', md: 'row'},
+                    gap: 5
+                }} maxWidth={'xl'}>
+                <Card>
+                    <CardMedia
+                        component="video"
+                        src={'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4'}
+                        controls={true}
+                    />
+                    <CardContent>
+                        <Typography gutterBottom variant="h5" component="div">
+                            Lizard
+                        </Typography>
+                        <Typography variant="body2" sx={{color: 'text.secondary'}}>
+                            Lizards are a widespread group of squamate reptiles, with over 6,000
+                            species, ranging across all continents except Antarctica
+                        </Typography>
+                    </CardContent>
+                    <CardActions>
+                        <Button size="small">Share</Button>
+                        <Button size="small">Learn More</Button>
+                    </CardActions>
+                </Card>
+                <Card>
+                    <CardMedia
+                        component="video"
+                        src={'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4'}
+                        controls={true}
+                    />
+                    <CardContent>
+                        <Typography gutterBottom variant="h5" component="div">
+                            Lizard
+                        </Typography>
+                        <Typography variant="body2" sx={{color: 'text.secondary'}}>
+                            Lizards are a widespread group of squamate reptiles, with over 6,000
+                            species, ranging across all continents except Antarctica
+                        </Typography>
+                    </CardContent>
+                    <CardActions>
+                        <Button size="small">Share</Button>
+                        <Button size="small">Learn More</Button>
+                    </CardActions>
+                </Card>
+                <Card >
+                    <CardMedia
+                        component="video"
+                        src={'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4'}
+                        controls={true}
+                    />
+                    <CardContent>
+                        <Typography gutterBottom variant="h5" component="div">
+                            Lizard
+                        </Typography>
+                        <Typography variant="body2" sx={{color: 'text.secondary'}}>
+                            Lizards are a widespread group of squamate reptiles, with over 6,000
+                            species, ranging across all continents except Antarctica
+                        </Typography>
+                    </CardContent>
+                    <CardActions>
+                        <Button size="small">Share</Button>
+                        <Button size="small">Learn More</Button>
+                    </CardActions>
+                </Card>
+            </Container>
+            {/* <Container maxWidth={'xl'}>
+                <Box flexDirection="row" display="flex" justifyContent="space-between">
+                    <Box padding={2}>
+                        <Box>
+                            <img src={'https://picsum.photos/500/500?random=3'}/>
+                        </Box>
+                        <Box padding={5}>
+                            <Box padding={1}>
+                                <Stack direction="row" spacing={1}>
+                                    <Chip label="C++" color="primary"/>
+                                    <Chip label="OpenGL" color="success"/>
+                                    <Chip label="SDL3" color="success"/>
+                                </Stack>
+                            </Box>
+                            <Box padding={1}>
+                                <Typography variant="h3">Ricochet Game</Typography>
+                                <Typography>Our Midstone project sample description</Typography>
+                            </Box>
+                            <Button variant="text" href={'https://github.com/Vakarian-S'} target={'_blank'}>Check it out on Github</Button>
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+                        </Box>
+                    </Box>
+                    <Box padding={2}>
+                        <Box>
+                           <video width={'500px'}  style={{ aspectRatio: 16/ 9}} controls src={'public/Game101.mp4'}></video>
+                        </Box>
+                        <Box padding={5}>
+                            <Box padding={1}>
+                                <Stack direction="row" spacing={1}>
+                                    <Chip label="C++" color="primary"/>
+                                    <Chip label="2D" color="success"/>
+                                    <Chip label="SDL3" color="success"/>
+                                </Stack>
+                            </Box>
+                            <Box padding={1}>
+                                <Typography variant="h3">Bouncing Balls</Typography>
+                                <Typography>My Physics homework</Typography>
+                            </Box>
+                        </Box>
+                    </Box>
+                    <Box padding={2}>
+                        <Box>
+                            <img src={'https://picsum.photos/500/500?random=2'}/>
+                        </Box>
+                        <Box padding={5}>
+                            <Box padding={1}>
+                                <Stack direction="row" spacing={1}>
+                                    <Chip label="C++" color="primary"/>
+                                    <Chip label="3D" color="success"/>
+                                    <Chip label="SDL3" color="success"/>
+                                </Stack>
+                            </Box>
+                            <Box padding={1}>
+                                <Typography variant="h3">Snooker Simulation</Typography>
+                                <Typography>Physics Homework</Typography>
+                            </Box>
+                        </Box>
+                    </Box>
+                </Box>
+            </Container>*/}
+        </div>
+    );
 }
 
-export default App
+export default App;
---
+++ src/surfaces/AppBar.tsx
@@ -0,0 +1,54 @@
+﻿import {
+    AppBar,
+    Box,
+    Container,
+    IconButton,
+    Toolbar,
+    Tooltip,
+    Typography
+} from "@mui/material";
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import DescriptionIcon from '@mui/icons-material/Description';
+
+function ResponsiveAppBar() {
+
+    return (
+        <AppBar position="sticky">
+            <Container maxWidth="xl">
+                <Toolbar disableGutters>
+                    <Box sx={{flexGrow: 1}}>
+                        <Typography
+                            variant="h2"
+                            fontWeight={"bold"}
+                            noWrap
+                            component="a"
+                            sx={{fontSize: {xs: '18px', md: '32px'}}}
+                        >
+                            Sebastian Villarroel
+                        </Typography>
+                    </Box>
+                    <Box sx={{flexGrow: 0}}>
+                        <Tooltip title="My LinkedIn">
+                            <IconButton aria-label="linkedin" size={"medium"} style={{color: 'white'}}>
+                                <LinkedInIcon fontSize={"large"}/>
+                            </IconButton>
+                        </Tooltip>
+                        <Tooltip title="My Github">
+                            <IconButton aria-label="github" size={"medium"} style={{color: 'white'}}>
+                                <GitHubIcon fontSize={"large"}/>
+                            </IconButton>
+                        </Tooltip>
+                        <Tooltip title="My Resume">
+                            <IconButton aria-label="resume" size={"medium"} style={{color: 'white'}}>
+                                <DescriptionIcon fontSize={"large"}/>
+                            </IconButton>
+                        </Tooltip>
+                    </Box>
+                </Toolbar>
+            </Container>
+        </AppBar>
+    );
+}
+
+export default ResponsiveAppBar;
